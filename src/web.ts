5ca70d7cb0c25949fe391ed787ba12557200041b
Lan Hao <whu.seven@gmail.com>
2021-07-20 11:55:35 +0800

    update README

--- src/web.ts
+++ src/web.ts
@@ -3,13 +3,6 @@ import { WebPlugin } from '@capacitor/core';
 import { MapNavigationPlugin, NavigationOptions, PermissionStatus } from './definitions';
 
 export class MapNavigationWeb extends WebPlugin implements MapNavigationPlugin {
-  constructor() {
-    super({
-      name: 'MapNavigationPlugin',
-      platforms: ['web', 'android']
-    });
-  }
-  
   async startNavigation(options: NavigationOptions): Promise<void> {
     throw new Error('功能在此平台不可用'+JSON.stringify(options));
   }
